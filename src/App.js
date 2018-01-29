9bc3cbbf9a02ba5d61f6f6eee00ec926024ccd96
stepan <stepan@cog1.com>
2018-01-29 21:55:32 +0000

    107 udemy

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  /**
+   * Renders the component.
+   */
+  render() {
+
+  return (
+      <div>
+        <h1>MyBurger</h1>
+      </div>
+    );
+  }
+
+}
+
+export default App;
