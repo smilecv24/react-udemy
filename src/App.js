efe1165fcf9661f66dd30e2841d4f96abb2927de
stepan <stepan@cog1.com>
2018-01-30 22:09:53 +0000

    109 udemy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   /**
@@ -6,10 +8,10 @@ class App extends Component {
    */
   render() {
 
-  return (
-      <div>
-        <h1>MyBurger</h1>
-      </div>
+    return (
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     );
   }
 
