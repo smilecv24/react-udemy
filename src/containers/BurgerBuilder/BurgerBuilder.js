3cb930d9cd30405df06a0af2ffa8bf1b59f720b9
stepan <stepan@cog1.com>
2018-02-08 20:29:01 +0000

    164 udemy

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salat: 0.5,
@@ -75,7 +76,25 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('continue');
+    // alert('continue');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Smile',
+        address: {
+          street: 'test Street',
+          zipCode: '12345678',
+          country: 'Ukraine'
+        },
+        email: 'test@test.com'
+      },
+      delivery: 'fastest'
+    };
+
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
   };
 
   render() {
