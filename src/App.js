dd6c0b5ae580b8b315c19c7776c9ca216774bced
stepan <stepan@cog1.com>
2018-02-11 14:16:02 +0000

    203 udemy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+
+import {Route, Switch} from 'react-router-dom';
+
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -20,11 +23,20 @@ class App extends Component {
   render() {
 
     return (
-      <Layout>
-        {this.state.show ? <BurgerBuilder/> : null}
-        <Checkout/>
-      </Layout>
-    );
+      /*{
+        /!*<Layout>
+                {this.state.show ? <BurgerBuilder/> : null}
+                <Checkout/>
+              </Layout>*!/
+      }*/
+    <Layout>
+      <Switch>
+        <Route path='/checkout' component={Checkout}/>
+        <Route path='/' exact component={BurgerBuilder}/>
+      </Switch>
+    </Layout>
+  )
+    ;
   }
 
 }
