c3876a4d5d76c2f0a610d6bca0571a6cf3945a89
stepan <stepan@cog1.com>
2018-02-11 13:36:08 +0000

    203 udemy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   state = {
     show: true
   };
 
-  componentDidMount() {
+  /*componentDidMount() {
     setTimeout(() => {
       this.setState({show: false});
     }, 5000);
-  }
+  }*/
 
   /**
    * Renders the component.
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <Layout>
         {this.state.show ? <BurgerBuilder/> : null}
+        <Checkout/>
       </Layout>
     );
   }
