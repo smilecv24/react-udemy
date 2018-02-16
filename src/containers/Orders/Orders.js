0c04dc00c6b55ec3e6566adb93d0a264801c82a9
stepan <stepan@cog1.com>
2018-02-16 21:01:32 +0000

    213 udemy

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
+class Orders extends Component {
+
+  state = {orders: [], loading: true};
+
+  componentDidMount() {
+    axios.get('/orders.json').then(response => {
+        const fetchedOrders = [];
+        for (let key in response.data) {
+          fetchedOrders.push({...response.data[key], id: key});
+        }
+        console.log(fetchedOrders);
+        this.setState({orders: fetchedOrders, loading: false});
+      }
+    ).catch(err => {
+      console.log(err);
+      this.setState({loading: false})
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.orders.map((order) => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default withErrorHandler(Orders, axios);
